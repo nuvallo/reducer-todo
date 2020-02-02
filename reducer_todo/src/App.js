65a8502a688cc36eb7059208610e9c4ffb4ddc8d
Tyler <nuvallo.inc@gmail.com>
2020-02-02 14:02:01 -0500

    Completed handlers

--- reducer_todo/src/App.js
+++ reducer_todo/src/App.js
@@ -1,9 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import "./App.css";
+import { reducer, initialState } from "./reducers/todoReducer";
 
 function App() {
-  const [input, setInput] = useState();
-  return null;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addTodo = input => {
+    const newTodo = {
+      todo: input,
+      completed: false,
+      id: Math.random()
+    };
+
+    dispatch({ type: "ADD_TODO", payload: newTodo });
+  };
+
+  const handleComplete = id => {
+    dispatch({ type: "COMPLETED_TODO", payload: id });
+  };
+
+  const clearComplete = id => {
+    dispatch({ type: "CLEAR_COMPLETED", payload: id });
+  };
+
+  return (
+    <div className="App">
+      <header>
+        {state.map(todo => (
+          <h1 key={todo.id}>{todo.name}</h1>
+        ))}
+      </header>
+    </div>
+  );
 }
 
 export default App;
